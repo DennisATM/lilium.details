5641b4c1551274b092b0d78f39e680379e05791f
Dennis Torres <dennisalbertotm@gmail.com>
2025-10-19 08:11:13 -0300

    update api url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1", 
+  baseURL: "https://lilium-api.onrender.com/api/v1", 
 });
 
 // Interceptor para enviar token JWT si existe
